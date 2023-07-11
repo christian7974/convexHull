7853985128dfb49e9d5f7ff8d5af0e32297571d1
christian7974 <ctropeano@uri.edu>
2023-07-11 10:24:38 -0400

    Added Click Functionality, Draw Oval
    Added functionality for canvas to listen to mouse clicks, draw oval where user clicked and can keep track of where the user clicked.

--- main.py
+++ main.py
@@ -2,9 +2,13 @@ from tkinter import *
 
 window = Tk()
 label = Label(text="First tkinter line")
-label.pack()
-
-window.geometry("400x400")
-
-window.maxsize(1000, 1000)
+label.place(x=100, y=200)
+canvas = Canvas(window, bg='white')
+# window.geometry("400x400")
+def callback(event):
+    print ("clicked at", event.x, event.y)
+    canvas.create_oval(event.x - 10, event.y - 10, event.x + 10, event.y + 10, outline='black', fill='black')
+canvas.bind("<Button-1>", callback)
+canvas.pack(fill=BOTH, expand=True)
+window.maxsize(800, 800)
 window.mainloop()
