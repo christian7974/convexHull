1f53071771c4c37ee82bbfbd47ddc6f0ac49d5fc
christian7974 <ctropeano@uri.edu>
2023-07-12 21:31:40 -0400

    Able to Sort Points by Polar Angle
    Able to sort the points by the polar angle from the lowest point; changed from points represented as tuples to a Point class.

--- main.py
+++ main.py
@@ -1,20 +1,29 @@
 from tkinter import *
+import math
+class Point:
+    def __init__(self, x, y, angle):
+        self.x = x
+        self.y = y
+        self.angle = angle
+
+    def __str__(self):
+        return str(self.x) + "," + str(self.y) + " angle: " + str(self.angle)
+
 XPOS = 0
 YPOS = 1
+POLAR_ANGLE = 2
 window = Tk()
-label = Label(text="First tkinter line")
-label.place(x=100, y=200) 
 points = []
-canvas = Canvas(window, bg='white')
+canvas = Canvas(window, bg='white', width=800, height=800)
 
 def drawButtons():
     create_hull.pack()
     clear_hull.pack()
-# window.geometry("400x400")
 def drawCircleAtMouseClick(event):
-    # print ("clicked at", event.x, event.y)
+    print ("clicked at", event.x, 800 - event.y)
     canvas.create_oval(event.x - 10, event.y - 10, event.x + 10, event.y + 10, outline='black', fill='black')
-    points.append((event.x, event.y))
+    canvas.create_text(event.x + 10, event.y + 20, text=str(event.x) + ", " + str(event.y), fill="black")
+    points.append(Point(event.x, event.y, 0))
 
 canvas.bind("<Button-1>", drawCircleAtMouseClick)
 
@@ -22,25 +31,42 @@ def findLeftmostLowPoint(pointA, pointB):
     two_lowest_points = [pointA, pointB]
     point_smallest_x = two_lowest_points[0]
     for j in two_lowest_points:
-        if (j[XPOS] < point_smallest_x[XPOS]):
+        if (j.x < point_smallest_x.x):
             pointB = j
         else:
             pointB = point_smallest_x
     return pointB
 
+def sortPointsByAngle(points, lowest_point):
+    lowest_point_theta = math.atan2(lowest_point.x, lowest_point.y)
+    for i in range(0, len(points)):
+        if (i != lowest_point):
+            second_theta = math.atan2(points[i].x, points[i].y)
+            r = (second_theta - lowest_point_theta) * (180 / math.pi)
+            if (r < 0):
+                r % 360
+            points[i].angle = r
+    points.sort(key=lambda x: x.angle)
+    return points
+    # return 0
+
 def findLowestPoint(list_of_points):
     point_lowest_y = list_of_points[0]
     for i in list_of_points:
-        if (i[YPOS] < point_lowest_y[YPOS]):
+        if (i.y < point_lowest_y.y):
             point_lowest_y = i
-        elif (i[YPOS] == point_lowest_y[YPOS]): # If there are two points with the same y-coordinate, then choose the left most point
+        elif (i.y == point_lowest_y.y): # If there are two points with the same y-coordinate, then choose the left most point
             point_lowest_y = findLeftmostLowPoint(i, point_lowest_y)
     return point_lowest_y
 
 def createHull():
     # Code to find the Convex Hull of the points goes here
     lowest_point = findLowestPoint(points)
-    print(lowest_point)
+    # print(lowest_point.x, lowest_point.y)
+    sorted_points_by_angle = sortPointsByAngle(points, lowest_point)
+    for i in sorted_points_by_angle:
+        print(i)
+   
 
 def clearHull():
     canvas.delete('all')
